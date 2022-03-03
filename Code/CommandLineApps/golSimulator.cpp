542cbe3700f6f77ca3679ae91da6b568ae40e5bf
Daniel Ding <dinsanity@foxmail.com>
2022-03-03 03:41:46 +0000

    Modified a lot of function naming and variable naming in the golSimulator.cpp and golStillLife.cpp files, added some comments and error reporting mechanisms, and simply optimized the implementation logic, and compiled them in cmake.

--- Code/CommandLineApps/golSimulator.cpp
+++ Code/CommandLineApps/golSimulator.cpp
@@ -12,74 +12,71 @@
 
 =============================================================================*/
 
-/**
- * \brief Demo file to check that includes and library linkage is correct.
- */
 #include "golGameOfLife.h"
 #include "golGameGrid.h"
 
-
-namespace gol {
-
-static void show_usage(std::string name)
-{
-    std::cerr << "Usage: " << name << " <option(s)> SOURCES"
-              << "Options:\n"
-              << "\t-h,--help\t\tShow this help message\n"
-              << "\t-d,--destination DESTINATION\tSpecify the destination path"
-              << std::endl;
-}
-
-
 int main(int argc,char *argv[]){
-    int gen = 0;
-    int pattern;
-    int wid, hei, num;
-    std::string filename; 
 
-    if (argc > 8) {
-        show_usage(argv[0]);
-        return 1;
-    }
+    try{
+        int gen = 0;
+        int pattern;
+        int wid, hei, num;
+        std::string filename;
 
-    if (argc == 3){
-        pattern = 0;
-        filename = argv[1];
-        gen = std::atoi(argv[2]);
-        
-    }else if(argc == 5){
-            
-        wid = std::atoi(argv[1]);
-        hei = std::atoi(argv[2]);
-        num = std::atoi(argv[3]);
-        gen = std::atoi(argv[4]);
+        if ((strcmp(argv[1], "--help")==0) || (strcmp(argv[1], "-h") == 0)) {
+            //If the input parameter is -h or --help
+            std::cout<<"Hello user! Welcome to the GameLife Simulation application. \n";
+            std::cout<<"You can choose one of the following two options for initializing your grid. \n";
+            std::cout<<" 1. Enter a text file. The input command has two parameters, the file path and the number of iterations to try. For example.\n";
+            std::cout<<" ./bin/gofSimulator/Testing/Data/glider.txt 10 \n";
+            std::cout<<" 2: Enter the grid parameters for the random initial conditions. The input command has 4 parameters, rows, columns, number of surviving cells and the number of iterations per grid. Starting with the contents of a random cell for the initial condition, the statement is as follows.\n";
+            std::cout<<" ./bin/gofSimulator 20 12 30 10 \n";
+            std::cout<<"More information is provided by the command --help ot -h\n";
+        }else if (argc == 3){
+            //If the number of input parameters is 2
+            pattern = 0;
+            filename = argv[1];
+            gen = std::atoi(argv[2]);
 
-        pattern = 1;
-    } 
+            gol::GameGrid gameGrid(filename);
+            gol::GameOfLife gameOfLife(&gameGrid);
+            for(int i = 0; i<gen;i++){
+                gameOfLife.PrintGridForGame();
+                gameOfLife.TakeStep();
+                gameOfLife.SwapGrids();
+                std::this_thread::sleep_for (std::chrono::seconds(1));
+            }
+        }else if(argc == 5){
+            //If the number of input parameters is 4
+            wid = std::atoi(argv[1]);
+            hei = std::atoi(argv[2]);
+            num = std::atoi(argv[3]);
+            gen = std::atoi(argv[4]);
+            //Check the plausibility of the input parameter data
+            if(wid<=0 || hei<=0 || num<0|| gen<0){
+                    std::cout<<"Should input integer > 0\n";
+                    throw std::runtime_error("value error of command line arguments");
+            }else if(num>wid*hei){
+                    std::cout<<"alive cells number should less than cells number\n";
+                    throw std::runtime_error("value error of command line arguments");
+            }
 
-    if(pattern){
-        world w(wid, hei, num);
-        w.show(); 
-        rule rll(&w);
-        for(int i = 0; i<gen;i++){
-            rll.showGame();
-            rll.applyRules();
-            rll.swapWrds();
-            std::this_thread::sleep_for (std::chrono::seconds(1));
+            gol::GameGrid gameGrid(wid, hei, num);
+            gol::GameOfLife gameOfLife(&gameGrid);
+            for(int i = 0; i<gen;i++){
+                gameOfLife.PrintGridForGame();
+                gameOfLife.TakeStep();
+                gameOfLife.SwapGrids();
+                std::this_thread::sleep_for (std::chrono::seconds(1));
+            }
+        }else{
+            std::cout<<"More information is provided by the instruction --help ot -h\n";
+            std::cout<<"\n"<<std::endl;
+            throw std::runtime_error("Command line arguments error");
         }
-    }else{
-        world w(filename);
-        w.show(); 
-        rule rll(&w);
-        for(int i = 0; i<gen;i++){
-            rll.showGame();
-            rll.applyRules();
-            rll.swapWrds();
-            std::this_thread::sleep_for (std::chrono::seconds(1));
-        }
+    }catch(std::exception& error){
+        std::cerr << "Caught Exception: " << error.what() << std::endl;
+        throw;
     }
-
     return 0;
-}
-
 }
--- Code/CommandLineApps/golStillLife.cpp
+++ Code/CommandLineApps/golStillLife.cpp
@@ -18,41 +18,64 @@
 #include "golGameOfLife.h"
 #include "golGameGrid.h"
 
-
-namespace gol {
-
-static void show_usage(std::string name)
-{
-    std::cerr << "Usage: " << name << " <option(s)> SOURCES"
-              << "Options:\n"
-              << "\t-h,--help\t\tShow this help message\n"
-              << "\t-d,--destination DESTINATION\tSpecify the destination path"
-              << std::endl;
-}
-
-
 int main( int argc, char* argv[] ) {
 
-    
-    int wid = std::atoi(argv[1]);
-    int hei = std::atoi(argv[2]);
-    int num = std::atoi(argv[3]);
-    int gen = std::atoi(argv[4]);
+    try{
+        int gen = 0;
+        int pattern;
+        int wid, hei, num;
+        std::string filename;
 
-    while(1){
-        world w(wid, hei, num);
-        rule rll(&w);
-        for(int i = 0; i<gen;i++){
-            rll.applyRules();
+        if ((strcmp(argv[1], "--help")==0) || (strcmp(argv[1], "-h") == 0)) {
+            //If the input parameter is -h or --help
+            std::cout<<"Hello user! Welcome to the GameLife Simulation application. \n";
+            std::cout<<"You can choose one of the following two options for initializing your grid. \n";
+            std::cout<<" 1. Enter a text file. The input command has two parameters, the file path and the number of iterations to try. For example.\n";
+            std::cout<<" ./bin/gofSimulator/Testing/Data/glider.txt 10 \n";
+            std::cout<<" 2: Enter the grid parameters for the random initial conditions. The input command has 4 parameters, rows, columns, number of surviving cells and the number of iterations per grid. Starting with the contents of a random cell for the initial condition, the statement is as follows.\n";
+            std::cout<<" ./bin/gofSimulator 20 12 30 10 \n";
+            std::cout<<"More information is provided by the command --help ot -h\n";
+        }else if (argc == 5){
+            //If the number of input parameters is 5
+            int wid = std::atoi(argv[1]);
+            int hei = std::atoi(argv[2]);
+            int num = std::atoi(argv[3]);
+            int gen = std::atoi(argv[4]);
+            //Check the plausibility of the input parameter data
+            if(wid<=0 || hei<=0 || num<0|| gen<0){
+                    std::cout<<"Should input integer > 0\n";
+                    throw std::runtime_error("value error of command line arguments");
+                }
+                else if(num>wid*hei){
+                    std::cout<<"alive cells number should less than cells number\n";
+                    throw std::runtime_error("value error of command line arguments");
+                }
 
-            if(rll.is_still_lifes() && rll.hasLivingCells()){
-                rll.showGame();
-                break;
+            // for each cell
+            for(int grid_id = 0; grid_id<2000; grid_id++){
+                gol::GameGrid gameGrid(wid, hei, num);
+                gol::GameOfLife gameOfLife(&gameGrid);
+                // all need to loop gen times
+                for(int i = 0; i<gen;i++){
+                    gameOfLife.TakeStep();
+                    // if the grid remains stationary and there are still cells alive
+                    if(gameOfLife.IsStillLifes() && gameOfLife.HasLivingCells()){
+                        gameOfLife.PrintGridForGame();
+                        break;
+                    }
+                    gameOfLife.SwapGrids();
+                }
             }
-            rll.swapWrds();
-            //std::this_thread::sleep_for (std::chrono::seconds(1));
+        }else{
+            std::cout<<"More information is provided by the instruction --h ot -h\n";
+            std::cout<<"\n"<<std::endl;
+            throw std::runtime_error("Command line arguments error");
         }
+    }catch(std::exception& error) {
+        std::cerr << "Caught Exception: " << error.what() << std::endl;
+        throw;
     }
+    return 0;
 }
 
-}
+
